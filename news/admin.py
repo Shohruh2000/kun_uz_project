a1b3e6625fd3197b83aa8afdf30c383ca74a9925
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2023-03-12 15:28:47 +0500

    models import Category,Region,News,Tag

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Region, News, Tag
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Region)
+admin.site.register(News)
+admin.site.register(Tag)
