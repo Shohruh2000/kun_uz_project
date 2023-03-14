2ae9513094d5413c7fa4a03e8d43faba9322708b
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2023-03-14 14:38:54 +0500

    list kurnishda kirgizdim

--- news/admin.py
+++ news/admin.py
@@ -3,7 +3,9 @@ from .models import Category, Region, News, Tag
 # Register your models here.
 
 
-admin.site.register(Category)
-admin.site.register(Region)
-admin.site.register(News)
-admin.site.register(Tag)
+# admin.site.register(Category)
+# admin.site.register(Region)
+# admin.site.register(News)
+# admin.site.register(Tag)
+
+admin.site.register([Category,Region,News,Tag])
