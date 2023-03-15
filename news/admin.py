b2ec1d2b697ccca7c25813712b97c39f4a205994
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2023-03-15 17:03:45 +0500

    uzgarish krtildi

--- news/admin.py
+++ news/admin.py
@@ -8,4 +8,32 @@ from .models import Category, Region, News, Tag
 # admin.site.register(News)
 # admin.site.register(Tag)
 
-admin.site.register([Category,Region,News,Tag])
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Region,RegionAdmin)
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','craeted_at','category','region']
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(News,NewsAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name",'slug']
+    preserve_filters = {'slug':('name',)}
+
+
+admin.site.register(Tag,TagAdmin)
